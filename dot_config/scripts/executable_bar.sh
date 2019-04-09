acad0b485cbd5313392ab914be178a62075ef847
Sam Wedgwood <sam@wedg.uk>
2019-04-09 07:16:29 +0100

    added desktops to lemonbar

--- dot_config/scripts/executable_bar.sh
+++
@@ -1,8 +0,0 @@
-MODULES="$HOME/.config/scripts/panels"
-
-while true; do
-    echo "%{l} $($MODULES/date.sh) \
-          %{c}$($MODULES/time.sh) \
-          %{r}$($MODULES/media.sh)   $($MODULES/volume.sh) "
-  sleep 0.1
-done
